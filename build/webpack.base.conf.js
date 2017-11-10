f9dcac50c78557626ec7d206a3e804585bc21999
zhaoqingqing <shzhaoqingqing@corp.netease.com>
2017-11-10 18:41:07 +0800

    no message

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,34 +4,39 @@ var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
-  return path.join(__dirname, '..', dir)//__dirname：当前模块的文件夹名称
+  return path.join(__dirname, '..', dir)//__dirname：当前模块的文件夹名称，在这里指/build
 }
-
+//console.log(__dirname)
 module.exports = {
   entry: {
     app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot, // webpack输出的目标文件夹路径（例如：/dist）
-    filename: '[name].js',// /dist/[name].js   ?????
+    filename: '[name]+66666.js',// /dist/[name].js   ?????解释：开发并没有产生真实文件，只是存在内存里，目录结构如前
     publicPath: process.env.NODE_ENV === 'production'
     //publicPath: "/assets/",
     // 输出解析文件的目录，url 相对于 HTML 页面
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath// webpack编译输出的发布路径（例如'//cdn.xxx.com/app/'）跟目录
   },
-  resolve: {
+  resolve: {//这些选项能设置模块如何被解析。webpack 提供合理的默认值
+  	  // 别名，方便引用模块，例如有了别名之后，
+    // import Vue from 'vue/dist/vue.common.js'可以写成 import Vue from 'vue'
     extensions: ['.js', '.vue', '.json'],
+    //创建 import 或 require 的别名，来确保模块引入变得更简单
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
     }
   },
-  module: {
-    rules: [
-      {
+  module: {//这些选项决定了如何处理项目中的不同类型的模块。
+    rules: [//创建模块时，匹配请求的规则数组。这些规则能够修改模块的创建方式。这些规则能够对模块(module)应用 loader，或者修改解析器(parser)。
+     //每个规则可以分为三部分 - 条件(condition)，结果(result)和嵌套规则(nested rule)。
+     {
         test: /\.vue$/,
         loader: 'vue-loader',
+        //ule.options 和 Rule.query 是 Rule.use: [ { options } ] 的简写
         options: vueLoaderConfig
       },
       {
@@ -66,3 +71,4 @@ module.exports = {
     ]
   }
 }
+
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,8 +7,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
+// 给每个入口页面(应用)加上dev-client，用于跟dev-server的热重载插件通信，实现热更新
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+//console.log( baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
@@ -18,18 +20,22 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
-    new webpack.DefinePlugin({
+    new webpack.DefinePlugin({//设置全局变量
       'process.env': config.dev.env
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+     // 开启webpack热更新功能
     new webpack.HotModuleReplacementPlugin(),
+      // webpack编译过程中出错的时候跳过报错阶段，不会阻塞编译，在编译结束后报错
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
+       // 自动将依赖注入html模板，并输出最终的html文件到目标文件夹
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
       inject: true
     }),
+    // friendly-errors-webpack-plugin用于更友好地输出webpack的警告、错误等信息
     new FriendlyErrorsPlugin()
   ]
 })
